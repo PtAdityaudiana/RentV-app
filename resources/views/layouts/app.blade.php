6bd7c6e767fbf865df474bdef24f331882d057d2
Aditya-Udiana <adityaudiana16@gmail.com>
2025-11-11 20:03:06 +0800

    fixed header

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,11 +14,15 @@
         @if(session('user_id'))
           <a href="{{ route('user.dashboard') }}">Dashboard</a>
           <a href="{{ route('user.logout') }}">Logout</a>
+        @elseif(session('admin_id'))
+          <a href="{{ route('admin.dashboard') }}">Admin Dashboard</a>
+          <a href="{{ route('admin.logout') }}">Logout</a>
         @else
           <a href="{{ route('user.login') }}">Login</a>
           <a href="{{ route('user.register') }}">Register</a>
+          <a href="{{ route('admin.login') }}">Admin</a>
         @endif
-        <a href="{{ route('admin.login') }}">Admin</a>
+        
       </nav>
     </div>
   </header>
