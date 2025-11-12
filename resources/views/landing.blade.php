be8108ec717118d88fe3498a55f6bdec322b297a
Aditya-Udiana <adityaudiana16@gmail.com>
2025-11-13 06:55:31 +0800

    tambah kolom price.

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,5 +4,5 @@
 
 @section('content')
   <h1>RENT-V</h1>
-  <p>Selamat datang. Sistem ini sederhana: user dapat melihat kendaraan, request peminjaman, dan admin akan approve/reject.</p>
+  <p>Selamat datang di Rent-V. Sistem peminjaman kendaraan: anda dapat melihat kendaraan, request peminjaman, dan admin akan approve/reject.</p>
 @endsection
